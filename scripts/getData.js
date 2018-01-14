3323ae8eb265df6a411cd5251776fe7114c887b2
nesdown <newsdownloads12@gmail.com>
2018-01-14 18:24:53 +0200

    Add minor updates

---
+++ scripts/getData.js
@@ -0,0 +1,38 @@
+'use strict';
+let data = '';
+  //data = String($('input').val());
+
+setTimeout(
+  function {
+    $('#output').data('Waiting for input.');
+  },
+  1000
+);
+
+setTimeout(
+  function {
+    $('#output').data('Waiting for input..');
+  },
+  1000
+);
+
+setTimeout(
+  function {
+    $('#output').data('Waiting for input...');
+  },
+  1000
+);
+
+setTimeout(
+  function {
+    $('#output').data('Waiting for input..');
+  },
+  1000
+);
+
+setTimeout(
+  function {
+    $('#output').data('Waiting for input.');
+  },
+  1000
+);
--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
 'use strict'
-let readline = require('readline');
+//let readline = require('readline');
 
 let sentence = '';
 let salt = {
@@ -9,20 +9,25 @@ let salt = {
   y: 'z', z: 'a'
 }
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
 
-rl.question('Enter the sentence to crypt: ', (answer) => {
+/*rl.question('Enter the sentence to crypt: ',*/
+function editData(answer) {
   console.log(`This will be crypted: ${answer}`);
   sentence = String(answer);
   sentence = sentence.toLowerCase();
+  console.log(`Sentence looks like this: ${sentence}`);
   sentence = sentence.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,'');
+  console.log(`Going to cifer: ${sentence}`);
   cipher(sentence);
-  rl.close();
-});
+  //rl.close();
+};
 
+//Here we've changed full function ionto lambda
+let start = () => (editData(String($('input').val())))
 
 function cipher(sentence) {
   let letters = sentence.split('');
@@ -35,4 +40,5 @@ function cipher(sentence) {
   }
 
   console.log(`Crypted sentence: ${crypted}`);
+  $('#output').text(String(crypted));
 }
